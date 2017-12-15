e848ac488c7573bab8f46d6f0b8620bcaacbf25c
Horace Keung <horacekeung@hotmail.com>
2017-12-15 22:03:55 +0000

    can log out now

--- components/app/AppFooter.vue
+++ components/app/AppFooter.vue
@@ -1,13 +1,20 @@
 <template>
-	<footer class="footer-shadow p-2 text-right">
-		<small>© {{currentYear}} Exantai Analytics</small>
+	<footer class="footer-shadow p-2 d-flex">
+		<div><small>© {{currentYear}} Exantai Analytics</small></div>
+		<div @click="logOut" class="ml-auto pointer d-flex red hoverDarkRed">
+			<i class="fa fa-sign-out my-auto" aria-hidden="true"></i><small>{{lang[38]}}</small>
+		</div>
 	</footer>
 </template>
 
 <script>
+import util from '~/assets/js/util'
 export default {
 	computed: {
 		currentYear () { return this.$store.state.currentYear }
+	},
+	methods: {
+		logOut () { util.logOut(this) }
 	}
 }
 </script>
